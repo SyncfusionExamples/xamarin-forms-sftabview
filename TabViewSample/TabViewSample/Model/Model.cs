cd53aba8c46213f7bcaae1f1ec04c05a4a143b86
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-27 13:06:02 +0530

    Getting started sample has been added

---
+++ TabViewSample/TabViewSample/Model/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TabViewSample
+{ 
+    public class ContactInfo
+    {
+        public string Name { get; set; }
+        public long Number { get; set; }
+    }
+}
